0af57ca9270a8f71d6cdd925b69d8df611df7b0d
stopyc <e13242597082@outlook.com>
2023-07-15 09:32:17 +0800

    feat:初始化项目骨架

---
+++ src/main/java/shop/stopyc/IdWorkerApplication.java
@@ -0,0 +1,16 @@
+package shop.stopyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YC104
+ */
+@SpringBootApplication
+public class IdWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/stopyc/IdWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.stopyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
